f1a32b32e23f1de86971546f91d516348f454f68
Albert Manyà <albertmp@eml.cc>
2014-11-09 11:27:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# photo-organizer: classify photos in folders by date looking at EXIF data
+
+## Install
+
+```
+ln -s exif-py/exifread
+```
+
+## Usage
+
+```
+python photo-organizer -i $INPUT_DIR -o $OUTPUT_DIR
+```
---
+++ photo-organizer.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+import getopt
+import exifread
+from dateutil.parser import parse
+
+def make_destination_path(output_dir, date):
+    return os.path.join(output_dir, str(date.year), str(date.month), str(date.day))
+
+def ensure_destination_is_created(destination_dir):
+    try:
+        os.makedirs(destination_dir)
+    except OSError, e:
+        if e.errno != 17: # file exists
+            raise OSError, e
+
+def is_bad_date(date):
+    p = re.compile(ur'\d{4}:\d{2}:\d{2} \d{2}:\d{2}:\d{2}')
+    if re.match(p, date):
+        return True
+    return False
+
+def fix_date(date):
+    return date[0:4] + '/' + date[5:6] + '/' + date[6:]
+
+def copy_file(src, dst):
+    print "copying %s to %s" % (src, dst)
+
+def process(input_dir, output_dir):
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            input_file = os.path.join(root, file)
+            f = open(input_file, 'rb')
+            tags = exifread.process_file(f)
+            try:
+                str_date = tags['Image DateTime']
+                print type(str_date)
+                if is_bad_date(str_date):
+                    str_date = fix_date(str_date)
+                date = parse(str_date)
+            except KeyError:
+                print "Not processing %s" % input_file
+            else:
+                destination_path = make_destination_path(output_dir, date)
+                ensure_destination_is_created(destination_path)
+                copy_file(input_file, os.path.join(destination_path, file))
+
+def usage(script_name):
+    print "%s -i <input_dir> -o <output_dir>" % script_name
+
+def main(argv):
+    input_dir = ''
+    output_dir = ''
+
+    try:
+        opts, args = getopt.getopt(argv[1:], "hi:o:", ["idir=", "odir="])
+    except getopt.GetoptError:
+        usage(argv[0])
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            usage(argv[0])
+            sys.exit()
+        elif opt in ("-i", "--idir"):
+            input_dir = arg
+        elif opt in ("-o", "--odir"):
+            output_dir = arg
+
+    if not os.path.isdir(input_dir):
+        print "bad input dir %s" % input_dir
+        sys.exit(2)
+
+    if not os.path.isdir(output_dir):
+        print "bad output dir %s" % output_dir
+        sys.exit(2)
+
+
+    process(input_dir, output_dir)
+
+if __name__ == '__main__':
+    main(sys.argv)
